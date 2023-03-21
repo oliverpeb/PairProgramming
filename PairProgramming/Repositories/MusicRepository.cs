573ee4b8f6da02f77ffb8b66a08612936eda55bd
Christopher Sloth Vesterby <chri414g@edu.easj.dk>
2023-03-21 10:45:44 +0100

    Chris tur 2!

--- PairProgramming/Repositories/MusicRepository.cs
+++ PairProgramming/Repositories/MusicRepository.cs
@@ -13,9 +13,46 @@ namespace PairProgramming.Repositories
             _music = new List<Music>()
             {
                 new Music() { Title = "Hej", Genre = "Rock", Artist = "Halløj", Id = 1},
+                new Music() { Title = "Farvel", Genre = "Pop", Artist = "Chris", Id = 2},
+                new Music() { Title = "Goddag", Genre = "Rap", Artist = "Oliver", Id = 3},
 
             };
+
         }
+
+        public List<Music> GetAll()
+        {
+           return new List<Music>(_music);
+        }
+
+            
+        public Music? GetbyID(int id)
+        {
+           return _music.Find(x => x.ID == id);
+
+        }
+
+
+        public Music Add(Music newMusic)
+        {
+            newMusic.Id = _nextID++;
+            _music.Add(newMusic);
+            return newMusic;
+
+        }
+
+        public Music? Delete(int id)
+        {
+            Music? foundMusic = GetbyID(id);
+            
+
+
+        }
+
+
+
+
+        
 
     }
 }
