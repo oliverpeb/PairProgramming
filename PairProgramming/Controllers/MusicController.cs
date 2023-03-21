c69ee1b8f6ad44c3730f221b5ea84e1676b0c488
oliverpeb <83925702+oliverpeb@users.noreply.github.com>
2023-03-21 11:22:13 +0100

    Fjernede Gamle Controller

--- PairProgramming/Controllers/MusicController.cs
+++ PairProgramming/Controllers/MusicController.cs
@@ -4,30 +4,5 @@ using PairProgramming.Repositories;
 
 namespace PairProgramming.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class MusicController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-        private readonly ILogger<MusicController> _logger;
-
-        public MusicController(ILogger<MusicController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<Music> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new Music
-            {
-                
-            })
-            .ToArray();
-        }
-    }
 }
