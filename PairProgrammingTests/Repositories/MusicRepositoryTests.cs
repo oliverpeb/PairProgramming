5a881b711103336d09f722cc63abd36d948503d3
Christopher S. Vesterby <vesterby@hotmail.dk>
2023-03-27 13:53:39 +0200

    Påbegyndt UnitTest af vores repository klasse

---
+++ PairProgrammingTests/Repositories/MusicRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PairProgramming.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgramming.Repositories.Tests
+{
+    [TestClass()]
+    public class MusicRepositoryTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
