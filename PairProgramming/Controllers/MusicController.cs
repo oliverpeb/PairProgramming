71b85296a467ce777671dbb0a3f77f579d5722d3
Christopher S. Vesterby <vesterby@hotmail.dk>
2023-03-27 12:58:10 +0200

    Jeg lavede vores Controller om til read/write, så vi har nemmere ved at lave vores controller

--- PairProgramming/Controllers/MusicController.cs
+++ PairProgramming/Controllers/MusicController.cs
@@ -1,8 +1,44 @@
-using Microsoft.AspNetCore.Mvc;
-using PairProgramming.Models;
-using PairProgramming.Repositories;
+﻿using Microsoft.AspNetCore.Mvc;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PairProgramming.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+        // GET: api/<MusicController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            
+        }
 
+        // GET api/<MusicController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            
+        }
+
+        // POST api/<MusicController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/<MusicController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/<MusicController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
 }
